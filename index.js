cddeabf1d1bb3b5cb3b18b5cb06102e52e36b5ee
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-18 19:00:19 +0500

    Creating Routing

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv"
 import morgan from "morgan";
 import { connectDb } from "./utils/data.js";
 import { DaTa } from "./model/user.model.js";
+import { userRouter } from "./router/userRouter.js";
+
 dotenv.config();
 const app = express()
 app.use(morgan())
 app.use(express.json())
 const port = process.env.PORT || 5000
 connectDb()
+
 app.get('/data',(req,res)=>{
     res.send(DaTa)
     console.log(DaTa);
@@ -17,7 +20,11 @@ app.get('/data',(req,res)=>{
 const data = [];
 app.post('/post-data',(req,res)=>{
     res.status(201).send({data:req.body,message:"data posted"})
-   
-    
-
+    data.push(req.body)
+    console.log(req.body)
+})
+app.use("/api/v1/user", userRouter);
+app.listen(port,()=>{
+    console.log(`hello backend is running on ${port}` )
+    console.log("database is connected successfully")
 })
---
+++ router/userRouter.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { createUser } from "../controller/userController.js";
+
+export const userRouter = express.Router();
+userRouter.post("/create", createUser);
